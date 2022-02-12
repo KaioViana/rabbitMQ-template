c3d535a1f59c704a745d514cc66e79194c5ae245
KaioViana <kaiu.viana@hotmail.com>
2022-02-12 05:29:15 -0300

    create structure schedule queues

---
+++ src/template6/RabbitMqScheduler.js
@@ -0,0 +1,82 @@
+import { connectToRabbitMQ } from './connectToRabbitMQ.js';
+
+class RabbitMqScheduler {
+  constructor() {
+  }
+
+  async init() {
+    try {
+      const {conn, channel} = await connectToRabbitMQ('amqp://admin:123456@localhost:5672');
+      this.conn = conn;
+      this.channel = channel;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  async handleScheduleMessage(queue, exchange, routingKey) {
+    try {
+      const { consumerCount } = await this.channel.checkQueue(queue);
+      console.log('ConsumerCount:', consumerCount);
+
+      if(consumerCount >= 1) {
+        setTimeout(() => {
+          console.log('this queue already have an consumer');
+          process.exit(0);
+        }, 500);
+        return;
+      }
+      await this.channel.assertExchange(exchange, 'x-delayed-message', {
+        autoDelete: false,
+        durable: true,
+        passive: true,
+        arguments: {
+          'x-delayed-type': 'topic'
+        }
+      });
+
+      const q = await this.channel.assertQueue(queue, { durable: true});
+      await this.channel.bindQueue(q.queue, exchange, routingKey);
+
+      await this.channel.prefetch(1);
+
+      await this.channel.consume(q.queue, (msg) => {
+        console.log(`routingKey: ${msg.fields.routingKey}, message ${msg.content.toString()}`)
+        this.channel.ack(msg);
+      }, { noAck: false });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async publishScheduleMessage(data, exchange, delay, queue, routingKey) {
+    try {
+      await this.channel.assertExchange(exchange, 'x-delayed-message', {
+        autoDelete: false,
+        durable: true,
+        passive: true,
+        arguments: {
+          'x-delayed-type': 'topic'
+        }
+      });
+
+      const q = await this.channel.assertQueue(queue, { durable: true });
+      await this.channel.bindQueue(q.queue, exchange, routingKey);
+
+      const message = JSON.stringify(data);
+
+      this.channel.publish(exchange, routingKey, Buffer.from(message), {
+        delivery: 2,
+        mandatory: true,
+        headers: {
+          'x-delay': delay,
+        },
+      });
+      console.log('PUBLISHED!');
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+export { RabbitMqScheduler };
---
+++ src/template6/main.js
@@ -0,0 +1,31 @@
+import { RabbitMqScheduler } from './RabbitMqScheduler.js'
+
+function run() {
+  const scheduler = new RabbitMqScheduler();
+  
+  const QUEUE_NAME = 'incorporadora_1';
+  const EXCHANGE = 'schedule_exchange';
+  const ROUTING_KEY = 'incorporadora_1.email'
+  const DELAY = 10000;
+  const MSG = {
+    index: 1,
+    content: 'CONTENT_TEST',
+    bonus: 'Congragulations you scheduled1'
+  }
+  
+  console.log(`QUEUE_NAME=${QUEUE_NAME}\nEXCHANGE=${EXCHANGE}\nROUTING_KEY=${ROUTING_KEY}\nDELAY=${DELAY}`);
+  
+  scheduler.init().then(() => {
+    scheduler.publishScheduleMessage(
+      MSG,
+      EXCHANGE,
+      DELAY,
+      QUEUE_NAME,
+      ROUTING_KEY
+    );
+
+    scheduler.handleScheduleMessage(QUEUE_NAME, EXCHANGE, ROUTING_KEY);
+  });
+};
+
+run();
